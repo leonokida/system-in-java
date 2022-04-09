245158a17a7044097079451df6d8e11fe55688d8
leonokida <le0n0kida@gmail.com>
2022-04-08 22:43:43 -0300

    [add] Lista de disciplinas cursadas - issue #1

---
+++ ListaCursadas.java
@@ -0,0 +1,37 @@
+package sistema.dinf;
+
+import java.lang.Integer;
+import java.util.Collections;
+import java.util.ArrayList;
+import sistema.dinf.DisciplinaCursada;
+
+public class ListaCursadas {
+
+    private ArrayList<DisciplinaCursada> lista;
+
+    public ListaCursadas() {
+        this.lista = new ArrayList<DisciplinaCursada>();
+    }
+
+    public void adiciona(DisciplinaCursada disciplina) {
+        this.lista.add(disciplina);
+    }
+
+    /*
+    Compara por semestre e por nome
+    */
+    public void ordena() {
+        this.lista.sort((o1, o2) -> {
+            int cmp = Integer.valueOf(o1.getSemestreCursado()).compareTo(o2.getSemestreCursado());
+            if (cmp != 0) {
+                return cmp;
+            }
+            return o1.getNome().compareTo(o2.getNome());
+        });
+    }
+
+    public DisciplinaCursada busca(int i) {
+        return this.lista.get(i);
+    }
+
+}
