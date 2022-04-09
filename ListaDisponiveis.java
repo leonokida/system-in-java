bc7f2b21ba3e3bb5c1095e935caf7d4cadef9564
leonokida <le0n0kida@gmail.com>
2022-04-08 22:50:51 -0300

    [add] Lista de disciplinas disponíveis - issue #2

---
+++ ListaDisponiveis.java
@@ -0,0 +1,37 @@
+package sistema.dinf;
+
+import java.lang.Integer;
+import java.util.Collections;
+import java.util.ArrayList;
+import sistema.dinf.DisciplinaDisponivel;
+
+public class ListaDisponiveis {
+
+    private ArrayList<DisciplinaDisponivel> lista;
+
+    public ListaDisponiveis() {
+        this.lista = new ArrayList<DisciplinaDisponivel>();
+    }
+
+    public void adiciona(DisciplinaDisponivel disciplina) {
+        this.lista.add(disciplina);
+    }
+
+    /*
+    Compara por período recomendado e por nome
+    */
+    public void ordena() {
+        this.lista.sort((o1, o2) -> {
+            int cmp = Integer.valueOf(o1.getPeriodo()).compareTo(o2.getPeriodo());
+            if (cmp != 0) {
+                return cmp;
+            }
+            return o1.getNome().compareTo(o2.getNome());
+        });
+    }
+
+    public DisciplinaDisponivel busca(int i) {
+        return this.lista.get(i);
+    }
+
+}
