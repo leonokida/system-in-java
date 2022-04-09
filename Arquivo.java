e7466398f4fdd33ee72804cf95ce6254c6dcc14f
vods <vinicius_ods@outlook.com>
2022-04-09 17:38:52 -0300

    classe arquivo atualizada para modo scanner, como vou dar casting nisso?

--- Arquivo.java
+++ Arquivo.java
@@ -1,33 +1,57 @@
 import java.io.*;
 import java.util.Vector;
+import java.util.Scanner;
 
 import sistema.dinf.Disciplina;
 import sistema.dinf.DisciplinaCursada;
 
 public class Arquivo {
 
-    public void Le_Disciplina_Cursada(DisciplinaCursada d){
-        try{
-            //generalizar nome do arquivo
-            FileInputStream arq = new FileInputStream("exemplo_trabalho_TAP_historico.csv");
-            ObjectInputStream in = new ObjectInputStream(arq);
-            d = (DisciplinaCursada) in.readObject();
+    public void Le_Disciplina_Cursada(ListaCursadas lista, DisciplinaCursada d){
+        //generalizar nome do arquivo
+        File arquivo = new File("exemplo_trabalho_TAP_historico.csv");
+        Scanner sc = new Scanner(arquivo);
+        String line; //string que vai pegar a linha 
+        int inicio; // posicao incial do atributo
+        int fim; //posicao final do atributo
+
+        while (sc.hasNext()) {
+            //leio a linha
+            line = sc.nextLine();
             
-            //Fazer com que leia mais de uma disciplina
-            //while(!d.getCodigo().equals(codigo)){
-            System.out.println(d.getCodigo());
-            System.out.println(d.getNome());
-            System.out.println(d.getCargaHoraria());
-            System.out.println(d.getDescr_estrutura());
-            System.out.println(d.getSituacao());
-            System.out.println(d.getSemestreCursado());
-            //}
-            in.close();
-        }catch(java.io.IOException exc2){
-            System.out.println("Erro ao Ler o arquivo");
-        }catch(ClassNotFoundException cnfex){
-            System.out.println("Não achou a Classe");
+            //atributo "codigo"
+            fim=line.indexOf(';');
+            d.setCodigo(line.substring (0, fim));
+            
+            //proximo atributo "nome"
+            inicio=fim+1;
+            fim=line.indexOf(';');
+            d.setNome(line.substring (inicio, fim));
+
+            //proximo atributo "carga horaria"
+            inicio=fim+1;
+            fim=line.indexOf(';');
+            d.setCargaHoraria(line.substring (inicio, fim));
+
+            //proximo atributo "descr_estrutura"
+            inicio=fim+1;
+            fim=line.indexOf(';');
+            d.setDescr_estrutura(line.substring (inicio, fim));
+
+            //proximo atributo "situacao"
+            inicio=fim+1;
+            fim=line.indexOf(';');
+            d.setSituacao(line.substring (inicio, fim));
+
+            //proximo atributo "semestre cursado"
+            inicio=fim+1;
+            fim=line.indexOf(';');
+            d.setSemestreCursado(line.substring (inicio, fim));
+            
+            lista.adiciona(d);
+
         }
+        sc.close();
     }
     
     public void Le_Disciplina_Disponivel(DisciplinaDisponivel d){
